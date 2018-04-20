24ddd36fa6786baf9a99c6d7c430d890853eaaf2
Nima Nouri <nima.nouri@yale.edu>
2018-04-20 17:54:16 -0400

    added.

---
+++ data-raw/GenerateExampleData.R
@@ -0,0 +1,39 @@
+# Generate example clones
+
+# Imports
+library(alakazam)
+
+#### Generate example database ####
+
+# Load data
+ExampleDb <- readChangeoDb("data-raw/ExampleDb.gz")
+ExampleDb <- ExampleDb[c("SEQUENCE_ID",
+                         "SEQUENCE_IMGT",
+                         "GERMLINE_IMGT_D_MASK",
+                         "V_CALL",
+                         "V_CALL_GENOTYPED",
+                         "D_CALL",
+                         "J_CALL",
+                         "JUNCTION",
+                         "JUNCTION_LENGTH",
+                         "NP1_LENGTH",
+                         "NP2_LENGTH",
+                         "SAMPLE",
+                         "ISOTYPE",
+                         "DUPCOUNT")]
+
+# Save
+devtools::use_data(ExampleDb, overwrite=TRUE)
+
+#### Generate example clones ####
+ClonedExampleDb <- defineClonesScope(db,
+                                     junction = "JUNCTION",
+                                     v_call = "V_CALL",
+                                     j_call = "J_CALL",
+                                     first = TRUE,
+                                     cdr3 = FALSE,
+                                     mod3 = FALSE,
+                                     nproc = 1,
+                                     progress = T)
+# Save
+devtools::use_data(ClonedExampleDb, overwrite=TRUE)
