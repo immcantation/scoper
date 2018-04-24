efe765b69efbc2fc0617caad24f674338542a803
Nima Nouri <nima.nouri@yale.edu>
2018-04-24 08:27:12 -0400

    added a section title to scope.R

--- R/scope.R
+++ R/scope.R
@@ -1,12 +1,20 @@
 # Project documentation and imports
 
-#' SCOPe
+#' The SCOPe package
 #'
 #' Provides a computational framework for unsupervised identification B cell
 #' clones from adaptive immune receptor repertoire sequencing (AIRR-Seq) datasets.
 #' This method is based on spectral clustering of the junction sequences of B cell
 #' receptors (BCRs, Immunoglobulins) that share the same V gene, J gene and
 #' junction length.
+#'
+#' @section Spectral Clustering for clOne Partitioning (SCOPe):
+#'
+#' \itemize{
+#'   \item  \link{defineClonesScope}:      Clustering sequences into clonal groups.
+#'   \item  \link{clonesAnalysis}:         Summary statistics and visualization of the
+#'                                         clonal clustering results.
+#' }
 #'
 #' @name        scope
 #' @docType     package
