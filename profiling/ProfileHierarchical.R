8f697e71126940ba1dee68ff134a1df4575edc0d
Susanna Marquez <20683719+ssnn-airr@users.noreply.github.com>
2022-05-23 20:44:07 +0200

    set up profiling

---
+++ profiling/ProfileHierarchical.R
@@ -0,0 +1,15 @@
+# Imports
+library(scoper)
+library(profvis)
+
+#### Load example data ####
+# setwd("profiling")
+data <- readr::read_tsv("1000.tsv", col_types = readr::cols())
+
+#### Calculate expected mutations ####
+profvis({
+    hierarchicalClones(data, threshold=0.12, junction = "JUNCTION",
+                       v_call = "V_CALL", j_call = "J_CALL", locus = "LOCUS",
+                       linkage = "single", normalize = "len", nproc = 1)
+})
+
